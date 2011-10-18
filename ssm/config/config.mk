663a81f0fe80d469096b65898141492ac1fc0678
armnrmt <armnrmt@198adb2e-9c79-420d-9e7c-cb98fabe6407>
2011-10-18 22:09:58 +0000

    correct commit of config subdir
    git-svn-id: svn://mrbsvn/others/vcoord/trunk@687 198adb2e-9c79-420d-9e7c-cb98fabe6407

---
+++ ssm/config/config.mk
@@ -0,0 +1,10 @@
+# this file contains common config that is sourced by all makefiles
+WHO_I_AM=$(shell whoami)
+VERSION=2.1.0
+MACHINE=$(shell uname -s)
+ARCH=$(MACHINE)
+SWNAME=vgriddescriptors
+SWDESCRIP='Vertical grid descriptors package'
+
+# platform specific definition
+SSMPACKAGE=$(SWNAME)_$(VERSION)_multi
