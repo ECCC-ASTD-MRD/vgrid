fc2f271a64e08b25b412ace0cd73972899f5e094
afsgapm <afsgapm@198adb2e-9c79-420d-9e7c-cb98fabe6407>
2013-04-18 18:04:07 +0000

    Correct memory leak found by Mark Buehner with call to vgd_get("NL_M",.. and vgd_get("NL_T",.. Also add key -convip to print_toctoc
    M    ssm/config/config.mk
    M    lib/vgrid_descriptors.ftn90
    M    bin/print_toctoc.ftn90
    A    bin/data/Linux/print_toctoc_convip_dm_5001_from_model_run.txt
    A    bin/data/Linux/print_toctoc_convip_dm_2001_5001_from_editfst_kind_2.txt
    A    bin/data/Linux/print_toctoc_convip_dm_1003_from_pgsm_lam_east_ops.txt
    A    bin/data/Linux/print_toctoc_convip_dm_2001_5001_from_editfst_kind_5.txt
    A    bin/data/Linux/print_toctoc_convip_2001_from_model_run.txt
    A    bin/data/Linux/print_toctoc_convip_dm_1002_from_model_run.txt
    A    bin/data/Linux/print_toctoc_convip_dm_1001_from_model_run.txt
    A    bin/data/AIX/print_toctoc_convip_dm_5001_from_model_run.txt
    A    bin/data/AIX/print_toctoc_convip_dm_2001_5001_from_editfst_kind_2.txt
    A    bin/data/AIX/print_toctoc_convip_dm_1003_from_pgsm_lam_east_ops.txt
    A    bin/data/AIX/print_toctoc_convip_dm_2001_5001_from_editfst_kind_5.txt
    A    bin/data/AIX/print_toctoc_convip_2001_from_model_run.txt
    A    bin/data/AIX/print_toctoc_convip_dm_1002_from_model_run.txt
    A    bin/data/AIX/print_toctoc_convip_dm_1001_from_model_run.txt
    M    bin/run_test_applications.ksh
    
    
    git-svn-id: svn://mrbsvn/others/vcoord/trunk@935 198adb2e-9c79-420d-9e7c-cb98fabe6407

--- ssm/config/config.mk
+++ ssm/config/config.mk
@@ -1,6 +1,6 @@
 # this file contains common config that is sourced by all makefiles
 WHO_I_AM=$(shell whoami)
-VERSION=4.0.0
+VERSION=4.3.0
 MACHINE=$(shell uname -s)
 ARCH=$(MACHINE)
 SWNAME=vgriddescriptors
