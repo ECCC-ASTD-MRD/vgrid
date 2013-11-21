1ebb1e1c95ac9b605bc0fb42ca3c4c05fad5f13e
afsgapm <afsgapm@198adb2e-9c79-420d-9e7c-cb98fabe6407>
2013-11-21 19:47:52 +0000

    Add vcode 5004 hybdrid staggered levels notop
    git-svn-id: svn://mrbsvn/others/vcoord/trunk@1009 198adb2e-9c79-420d-9e7c-cb98fabe6407

--- ssm/config/config.mk
+++ ssm/config/config.mk
@@ -1,6 +1,6 @@
 # this file contains common config that is sourced by all makefiles
 WHO_I_AM=$(shell whoami)
-VERSION=4.3.0
+VERSION=4.4.0-a1
 MACHINE=$(shell uname -s)
 ARCH=$(MACHINE)
 SWNAME=vgriddescriptors
