48440e6d0473e78093360cffee9af73c01ce593a
afsgapm <afsgapm@198adb2e-9c79-420d-9e7c-cb98fabe6407>
2014-04-03 18:02:59 +0000

    For version 5.0.1, add pgi9xx
    M    5.0/ssm/config/config.mk
    M    5.0/setup.dot
    
    
    git-svn-id: svn://mrbsvn/others/vcoord/branches/5.0@1170 198adb2e-9c79-420d-9e7c-cb98fabe6407

--- ssm/config/config.mk
+++ ssm/config/config.mk
@@ -1,8 +1,8 @@
 # this file contains common config that is sourced by all makefiles
 # Warning : no underscore '_' in CURRENT_VERSION string
-VERSION=5.0.0
+VERSION=5.0.1
 SSM_VERSION=$(VERSION)-$(COMP_ARCH)
-SWNAME=vgrid
+SWNAME=vgriddescriptors
 SWDESCRIP='Vertical grid descriptors package'
 
 SSMPACKAGE=$(SWNAME)_$(SSM_VERSION)_${PLATFORM}
