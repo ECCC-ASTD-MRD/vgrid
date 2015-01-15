3914f14a911cc001fa24d6a62679c107852a9d54
afsgapm <afsgapm@198adb2e-9c79-420d-9e7c-cb98fabe6407>
2015-01-15 18:24:56 +0000

    Use rmn 15.1, modif Makefile to remove s.compile key -librmn, correct bug with call to incdatr with dateo=0.
    M    tests/Makefile.tmpl
    M    setup.dot
    M    lib/vgrid_descriptors.ftn90
    M    lib/Makefile
    M    bh_vgrid.py
    
    
    git-svn-id: svn://mrbsvn/others/vcoord/tags/release-5.3.0_debug@1351 198adb2e-9c79-420d-9e7c-cb98fabe6407

--- bh_vgrid.py
+++ bh_vgrid.py
@@ -9,29 +9,28 @@ from bh import bhlib, actions
 def _init(b):
    environ["BH_PROJECT_NAME"] = "vgriddescriptors"
    environ["BH_PACKAGE_NAMES"] = "vgriddescriptors"
-   environ["BH_PACKAGE_VERSION"] = "5.3.0-a2"
-   environ["BH_PULL_SOURCE"] = "/users/dor/afsg/apm/data/vcoord/tags/release-5.3.0-a2"
-   #environ["RCOMPILE"] = "s.compile"
+   environ["BH_PACKAGE_VERSION"] = "5.3.1"
+   environ["BH_PULL_SOURCE"] = "/users/dor/afsg/apm/data/vcoord/tags/release-5.3.1"
    b.shell("""export RCOMPILE=s.compile""", environ)
-   b.shell("""export RMN=rmn_015""", environ)
    environ["DISTINATION_MACH"] = "pollux"
    environ["DISTINATION_DIR"] = "/users/dor/afsg/apm/ords/vgrid/%(BH_PACKAGE_VERSION)s" % environ
+   environ["RMN_VERSION"] = "15.1" % environ
 
    if b.mode == "intel":
-       b.shell(""". ssmuse-sh -d hpcs/201402/01/base -d hpcs/201402/01/intel13sp1u2 -d rpn/libs/15.0 """, environ)
+       b.shell(""". ssmuse-sh -d hpcs/201402/01/base -d hpcs/201402/01/intel13sp1u2 -d rpn/libs/${RMN_VERSION}""", environ)
        environ["FC"] = "intel13sp1u21"
        environ["BH_MAKE"] = 'make'
    elif b.mode == "pgi14":
-       b.shell(""". ssmuse-sh -d hpcs/201402/01/base -d hpcs/201402/01/pgi1401      -d rpn/libs/15.0""", environ)
+       b.shell(""". ssmuse-sh -d hpcs/201402/01/base -d hpcs/201402/01/pgi1401      -d rpn/libs/${RMN_VERSION}""", environ)
        environ["FC"] = "pgi1401"
        environ["BH_MAKE"] = 'make' 
    elif b.mode == "pgi9xx":
        b.shell(""". ssmuse-sh -d hpcs/13b/03/base -d rpn/libs/4.0;.  s.ssmuse.dot pgi9xx devtools""", environ)
-       b.shell("""export RMN=rmn_014""", environ)
+       b.shell("""export RMN_EXT=_014""", environ)
        environ["FC"] = "pgi9xx"
        environ["BH_MAKE"] = 'make' 
    elif b.mode == "xlf13":
-       b.shell(""". ssmuse-sh -d hpcs/201402/00/base -d hpcs/ext/xlf_13.1.0.10      -d rpn/libs/15.0""", environ)
+       b.shell(""". ssmuse-sh -d hpcs/201402/00/base -d hpcs/ext/xlf_13.1.0.10      -d rpn/libs/${RMN_VERSION}""", environ)
        environ["FC"] = "xlf"
        environ["BH_MAKE"] = 'gmake' 
 
@@ -42,7 +41,7 @@ if __name__ == "__main__":
    b.actions.set("pull", actions.pull.copy_dir)
    b.actions.set("clean", ["""(cd ${BH_BUILD_DIR}; ${BH_MAKE} distclean)"""])
    b.actions.set("make", actions.make.make)
-   #b.actions.set("test",["""(cd ${BH_BUILD_DIR}/tests; ${BH_MAKE} tests)"""])
+   b.actions.set("test",["""(cd ${BH_BUILD_DIR}/tests; ${BH_MAKE} tests)"""])
    b.actions.set("package",["""(cd ${BH_BUILD_DIR}/ssm; ${BH_MAKE})"""])
 
    b.supported_platforms = [
@@ -58,3 +57,6 @@ if __name__ == "__main__":
       "pgi9xx",
       "xlf13",
    ]
+
+# Exemple d'appel :
+# ./bh_vgrid.py -p ubuntu-12.04-amd64-64 -m intel
