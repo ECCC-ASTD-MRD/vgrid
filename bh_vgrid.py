24beb893e7154dc81f0affa964cb3a847b6cb221
Andre Plante <andre.plante@ec.gc.ca>
2015-03-17 20:04:19 +0000

    Adjust lib version number from libdescrip.a_x.y.z to libdescrip_x.y.z.a. Adjust VGRIDDESCRIPTORS_VERSION from x.y.z-platform to x.y.z. add VGRIDDESCRIPTORS_VERSION_COMP=x.y.z-platform

--- bh_vgrid.py
+++ bh_vgrid.py
@@ -10,7 +10,7 @@ def _init(b):
    # Adjust the folowing
    environ["BH_PROJECT_NAME"] = "vgriddescriptors"
    environ["BH_PULL_SOURCE"] = "%(BH_HERE_DIR)s" % environ
-   environ["BH_PULL_SOURCE_GIT_BRANCH"] = "5.3.2"
+   environ["BH_PULL_SOURCE_GIT_BRANCH"] = "5.4.0"
 
    # Add compiler if needed, select rmn lib version
    if b.mode == "intel":
@@ -66,8 +66,8 @@ def _install(b):
          set -e        
          mkdir -p ${BH_INSTALL_DIR}/lib
          cd ${BH_INSTALL_DIR}/lib
-         cp ${BH_TOP_BUILD_DIR}/lib/libdescrip.a libdescrip.a_${BH_PULL_SOURCE_GIT_BRANCH}
-         ln -s libdescrip.a_${BH_PULL_SOURCE_GIT_BRANCH} libdescrip.a
+         cp ${BH_TOP_BUILD_DIR}/lib/libdescrip.a libdescrip_${BH_PULL_SOURCE_GIT_BRANCH}.a
+         ln -s libdescrip_${BH_PULL_SOURCE_GIT_BRANCH}.a libdescrip.a
          mkdir -p ${BH_INSTALL_DIR}/include
          cd ${BH_INSTALL_DIR}/include
          cp ${BH_TOP_BUILD_DIR}/lib/*.mod .
