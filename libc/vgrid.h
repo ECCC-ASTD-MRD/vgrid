90ec025867fef3842aa90b4499bace689fbee317
Andre Plante <andre.plante@ec.gc.ca>
2015-05-15 18:31:39 +0000

    Ajout get_int get_int_1d get_real8_1d

--- libc/vgrid.h
+++ libc/vgrid.h
@@ -45,8 +45,8 @@ typedef struct TVGrid {
   double   *b_t_8;        // B-coefficients for thermodynamic levels
   int      *ip1_m;        // ip1 values for momentum levels
   int      *ip1_t;        // ip1 values for momentum levels
-  int      abi_m_nk;      //    size of a_m_8, b_m_8 and ip1_m
-  int      abi_t_nk;      //    size of a_t_8, b_t_8 and ip1_t
+  int      nl_m;          // Number of momentum      level (size of a_m_8, b_m_8 and ip1_m)
+  int      nl_t;          // Number ot thermodynamic level (size of a_t_8, b_t_8 and ip1_t)
   char*    ref_name;      // Reference field name
   float    rcoef1;        // Rectification coefficient
   float    rcoef2;        // Rectification coefficient
@@ -66,7 +66,7 @@ int c_new_gen(TVGrid **self, int kind, int version, float *hyb, int size_hyb, fl
 	      int *ip1, int *ip2, int *stdout_unit, float *dhm, float *dht);
 
 int c_new_build_vert(TVGrid **self, int kind, int version, int nk, int *ip1, int *ip2, double *ptop_8, double *pref_8, float *rcoef1, float *rcoef2, 
-		     double *a_m_8, double *b_m_8, double *a_t_8, double *b_t_8, int *ip1_m, int *ip1_t, int abi_m_nk, int abi_t_nk);
+		     double *a_m_8, double *b_m_8, double *a_t_8, double *b_t_8, int *ip1_m, int *ip1_t, int nl_m, int nl_t);
 
 int c_vgrid_genab_1001(float *hyb, int nk, float **hybm, double **a_m_8, double **b_m_8, int **ip1_m);
 
