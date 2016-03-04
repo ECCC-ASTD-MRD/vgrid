98bd1c77c236b3cdea1dd1846188536832e85496
Andre Plante <andre.plante@ec.gc.ca>
2016-03-04 16:13:34 +0000

    Add missing C_compute_pressure_2001 (float interface) and test

--- src/vgrid.c
+++ src/vgrid.c
@@ -704,32 +704,12 @@ static int C_compute_pressure_1001_1002(vgrid_descriptor *self, int ni, int nj, 
 }
 
 static int C_compute_pressure_2001_8(vgrid_descriptor *self, int ni, int nj, int nk, int *ip1_list, double *levels, int in_log) {
-  
-  int k,*ind,ij,ijk;
-  double lvl;
-  
-  if( my_alloc_int(&ind, nk, "(Cvgd) ERROR in C_compute_pressure_2001_8, cannot allocate ind of int of size\n") == VGD_ERROR )
-    return(VGD_ERROR);
-  
-  // Find ip1 indexes
-  for( k = 0; k < nk; ++k ){
-    if( ( ind[k] = VGD_FindIp1Idx(ip1_list[k],self->ip1_m,self->nl_m)) == -1 ) {
-      printf("(Cvgd) ERROR in C_compute_pressure_2001_8, cannot find ip1 %d in vgrid descriptor.\n",ip1_list[k]);
-      free(ind);
-      return(VGD_ERROR);
-    }
-  }
-  
-  // Compute pressure
-  for( k = 0, ijk=0; k < nk; ++k ){
-    for( ij = 0; ij < ni*nj; ++ij, ++ijk ){
-      lvl = self->a_m_8[ind[k]];
-      levels[ijk] = in_log ? log(lvl) : lvl;
-    }
-  }
-  free(ind);
-  return(VGD_OK);
-
+  char proc_name[] = "C_compute_pressure_2001_8";
+#include "BODY_C_compute_pressure_2001.hc"
+}
+static int C_compute_pressure_2001(vgrid_descriptor *self, int ni, int nj, int nk, int *ip1_list, float *levels, int in_log) {
+  char proc_name[] = "C_compute_pressure_2001";
+#include "BODY_C_compute_pressure_2001.hc"
 }
 
 static int C_compute_pressure_1003_5001_8(vgrid_descriptor *self, int ni, int nj, int nk, int *ip1_list, double *levels, double *sfc_field, int in_log, int dpidpis ){
@@ -741,7 +721,6 @@ static int C_compute_pressure_1003_5001(vgrid_descriptor *self, int ni, int nj, 
   char proc_name[] = "C_compute_pressure_1003_5001";
 #include "BODY_C_compute_pressure_1003_5001.hc"
 }
-
 
 static int C_compute_pressure_5002_5003_5004_5005_8(vgrid_descriptor *self, int ni, int nj, int nk, int *ip1_list, double *levels, double *sfc_field, int in_log, int dpidpis) {
   char proc_name[] = "C_compute_pressure_5002_5003_5004_5005_8";
