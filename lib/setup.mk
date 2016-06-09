39cedf56b130cdb7ce7406cb448e936e0b6f26e5
Andre Plante <andre.plante@ec.gc.ca>
2016-06-09 14:37:31 +0000

    Introduce suport to compile on gpsc with intel 16

--- lib/setup.mk
+++ lib/setup.mk
@@ -1,11 +1,12 @@
-#MY_FFLAGS = -O2
-#MY_FFLAGS = -C -g -O0
-#MY_CFLAGS = -O2
-#MY_CFLAGS =    -g -O0
+unset MY_FFLAGS
+
+MY_FFLAGS="-O2"
+#MY_FFLAGS="-C -g -O0"
+
+. version.dot
 
 HPCS_BASE=hpcs/201402/02/base/
 RPN_LIB=rpn/libs/15.2
-BH_PULL_SOURCE_GIT_BRANCH=5.6.1
 
 ifeq ($(ORDENV_PLAT),ubuntu-12.04-amd64-64)
    BH_MAKE=make
@@ -13,8 +14,7 @@ ifeq ($(ORDENV_PLAT),ubuntu-12.04-amd64-64)
    BH_MODE=intel
    COMPILER=intel13sp1u2
    HPCS_COMP = hpcs/201402/02/$(COMPILER)
-   FFLAGS = '$(MY_FFLAGS) -openmp -fp-model source  -warn all'
-   CFLAGS = '$(MY_CFLAGS) -openmp -fp-model precise -Wall'
+   MY_FFLAGS = '$(MY_FFLAGS) -openmp -fp-model source  -warn all'
 endif
 
 ifeq ($(ORDENV_PLAT),ubuntu-14.04-amd64-64)
@@ -23,8 +23,7 @@ ifeq ($(ORDENV_PLAT),ubuntu-14.04-amd64-64)
    BH_MODE=intel
    COMPILER=intel13sp1u2
    HPCS_COMP = hpcs/201402/02/$(COMPILER)
-   FFLAGS = '$(MY_FFLAGS) -openmp -fp-model source  -warn all'
-   CFLAGS = '$(MY_CFLAGS) -openmp -fp-model precise -Wall'
+   MY_FFLAGS = '$(MY_FFLAGS) -openmp -fp-model source  -warn all'
 endif
 
 ifeq ($(ORDENV_PLAT),aix-7.1-ppc7-64)
@@ -33,6 +32,7 @@ ifeq ($(ORDENV_PLAT),aix-7.1-ppc7-64)
    BH_MODE=xlf13
    COMPILER=xlf_13.1.0.10
    HPCS_COMP = hpcs/ext/$(COMPILER)
-   FFLAGS = '$(MY_FFLAGS)'
-   CFLAGS = '$(MY_FFLAGS)'
+   MY_FFLAGS = '$(MY_FFLAGS)'
 endif
+
+export MY_FFLAGS
