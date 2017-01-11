e390d5df12aa7cc1dd3750a65e6f8a5a5ad945e4
Andre Plante <andre.plante@ec.gc.ca>
2017-01-11 14:34:40 +0000

    Add test for vcode 5100

--- tests/src/levels_toplevel_value.F90
+++ tests/src/levels_toplevel_value.F90
@@ -60,7 +60,7 @@ program tests
 
   integer :: stat,lu=10,do_it,fstfrm,fclos,i,ier
   logical :: ok=.true.
-  integer, parameter :: nfiles=8
+  integer, parameter :: nfiles=9
   character(len=200), dimension(nfiles) :: files=(/&
        "data/dm_1001_from_model_run",&
        "data/dm_1002_from_model_run",&
@@ -69,7 +69,8 @@ program tests
        "data/dm_5002_from_model_run",&
        "data/dm_5003_from_model_run",&
        "data/dm_5004_from_model_run",&
-       "data/dm_5005_from_model_run"&
+       "data/dm_5005_from_model_run",&
+       "data/dm_5100_from_model_run"&
        /)
   
   call msg_verbosity(MSG_DEBUG)
