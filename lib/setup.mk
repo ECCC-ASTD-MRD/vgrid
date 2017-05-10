3b59fbd4e30fe2ca326994c6faba663e3f89c6e6
Andre Plante <andre.plante@ec.gc.ca>
2017-05-10 19:25:30 +0000

    Finish removal of BH_HOST

--- lib/setup.mk
+++ lib/setup.mk
@@ -10,7 +10,6 @@ RPN_LIB=rpn/libs/15.2
 
 ifeq ($(ORDENV_PLAT),ubuntu-12.04-amd64-64)
    BH_MAKE=make
-   BH_HOST=pollux
    BH_MODE=intel
    COMPILER=intel13sp1u2
    HPCS_COMP = hpcs/201402/02/$(COMPILER)
@@ -19,7 +18,6 @@ endif
 
 ifeq ($(ORDENV_PLAT),ubuntu-14.04-amd64-64)
    BH_MAKE=make
-   BH_HOST=einstein
    BH_MODE=intel
    COMPILER=intel13sp1u2
    HPCS_COMP = hpcs/201402/02/$(COMPILER)
@@ -28,7 +26,6 @@ endif
 
 ifeq ($(ORDENV_PLAT),aix-7.1-ppc7-64)
    BH_MAKE=gmake
-   BH_HOST=hadar
    BH_MODE=xlf13
    COMPILER=xlf_13.1.0.10
    HPCS_COMP = hpcs/ext/$(COMPILER)
