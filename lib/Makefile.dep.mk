6b832eedaf24cf5b4e4e465640f1d600f2335dde
Andre Plante <andre.plante@ec.gc.ca>
2017-08-25 13:44:27 +0000

    Remove test.o in Makefile.dep.mk add modelutils in setup

--- lib/Makefile.dep.mk
+++ lib/Makefile.dep.mk
@@ -1,25 +1,23 @@
-FTN90DECKS=
-INCDECKS=
-CDK90DECKS=
+TMPL90DECKS=
+HDECKS=
+F90DECKS= \
+	vgrid_descriptors.F90  vgrid_genab_1001.F90  vgrid_genab_1002_5001.F90  vgrid_genab_1004.F90  vgrid_genab_2001.F90   \
+	vgrid_genab_3001.F90  vgrid_genab_5002.F90  vgrid_genab_5100.F90  vgrid_utils.F90  
 PTN90DECKS=
 PTNDECKS=
+INCDECKS=
+FTNDECKS=
+FDECKS=
+FHDECKS=
+CDECKS=
+FTN90DECKS=
+CDKDECKS=
 HFDECKS= \
 	vgrid.hf  
-CDECKS=
-HDECKS=
-FHDECKS=
-FTNDECKS=
-F90DECKS= \
-	test.F90  vgrid_descriptors.F90  vgrid_genab_1001.F90  vgrid_genab_1002_5001.F90  vgrid_genab_1004.F90   \
-	vgrid_genab_2001.F90  vgrid_genab_3001.F90  vgrid_genab_5002.F90  vgrid_genab_5100.F90  vgrid_utils.F90  
-TMPL90DECKS=
-FDECKS=
-CDKDECKS=
+CDK90DECKS=
 OBJECTS= \
-	test.o  vgrid_descriptors.o  vgrid_genab_1001.o  vgrid_genab_1002_5001.o  vgrid_genab_1004.o   \
-	vgrid_genab_2001.o  vgrid_genab_3001.o  vgrid_genab_5002.o  vgrid_genab_5100.o  vgrid_utils.o  
-test.o:	test.F90
-
+	vgrid_descriptors.o  vgrid_genab_1001.o  vgrid_genab_1002_5001.o  vgrid_genab_1004.o  vgrid_genab_2001.o   \
+	vgrid_genab_3001.o  vgrid_genab_5002.o  vgrid_genab_5100.o  vgrid_utils.o  
 vgrid_descriptors.o:	vgrid_descriptors.F90 \
 	vgrid.hf  vgrid_genab_1001.o  vgrid_genab_1002_5001.o  vgrid_genab_1004.o  vgrid_genab_2001.o   \
 	vgrid_genab_3001.o  vgrid_genab_5002.o  vgrid_genab_5100.o  vgrid_utils.o  
@@ -43,7 +41,7 @@ vgrid_genab_5002.o:	vgrid_genab_5002.F90 \
 	vgrid.hf  vgrid_utils.o  
 
 vgrid_genab_5100.o:	vgrid_genab_5100.F90 \
-	vgrid.hf  
+	vgrid.hf  vgrid_utils.o  
 
 vgrid_utils.o:	vgrid_utils.F90 \
 	vgrid.hf  
