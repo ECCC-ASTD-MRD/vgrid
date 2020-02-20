db0214329bb9b3321d5812328eb3cd03de9c49f6
Jeff Blezius <jeffrey.blezius@canada.ca>
2020-02-20 12:39:41 -0500

    Pencil in the interface for another vcode

--- src/vgrid_interfaces.F90
+++ src/vgrid_interfaces.F90
@@ -5,3 +5,22 @@ module vgrid_5100
     module procedure vgd_compute_pressures_5100
   end interface vgd_levels_NewIfc
 end module vgrid_5100
+
+
+!module vgrid_5001_5005
+!  use Vgrid_descriptors, only: vgd_compute_pressures_5001_5005
+!
+!  interface vgd_levels_NewIfc
+!    module procedure vgd_compute_pressures_5001_5005
+!  end interface vgd_levels_NewIfc
+!end module vgrid_5001_5005
+
+
+module vgrid_generic
+  use Vgrid_descriptors
+
+  interface vgd_levels_NewIfc
+    module procedure vgd_compute_pressures_5100
+  ! module procedure vgd_compute_pressures_5001_5005  ! Also for 1003
+  end interface vgd_levels_NewIfc
+end module vgrid_generic
