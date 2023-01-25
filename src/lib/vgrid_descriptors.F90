d3da6826a5289af43d338f5874c00831948a825f
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2023-01-25 16:29:05 +0000

    Missed some quiet message

--- src/lib/vgrid_descriptors.F90
+++ src/lib/vgrid_descriptors.F90
@@ -468,15 +468,14 @@ contains
          
          if( f_new_read(self%cptr, unit, l_datev, l_etiket//C_NULL_CHAR, l_ip1, l_ip2, l_ip3, l_kind, l_version, l_quiet) == &
               VGD_ERROR )then
-!            write(app_msg,*) 'in new_read, problem with f_new_read'
-            call Lib_Log(APP_LIBVGRID,APP_ERROR,' in new_read, problem with f_new_read')       
+            if(.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,'new_read: problem with f_new_read')       
             return
          endif
       case ('BIN')
          read(unit) ni,nj,nk
          allocate(table_8(ni,nj,nk),stat=istat)
          if (istat /= 0) then
-            write(app_msg,*) ' in new_read unable to allocate table_8'
+            write(app_msg,*) 'new_read: unable to allocate table_8'
             call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
             return
          endif
@@ -484,13 +483,13 @@ contains
          
          error = new_from_table(self,table_8)
          if (error < 0) then
-            write(app_msg,*) ' in new_read, problem creating record information'
+            write(app_msg,*) 'new_read: problem creating record information'
             call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
             return
          endif
          ! Warn user on invalid input format specification
       case DEFAULT
-         write(app_msg,*) ' in new_read invalid constructor format request ',trim(myformat)
+         write(app_msg,*) 'new_read: invalid constructor format request ',trim(myformat)
          call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
@@ -830,7 +829,7 @@ contains
          value = l_value /= 0
       case DEFAULT
          write(app_msg,*) 'invalid key in call to getopt_logical: ',trim(key)
-         call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+         if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
       ! Set status and return
@@ -1415,7 +1414,7 @@ contains
          status = f_get_int_1d(self%cptr,my_key//C_NULL_CHAR,value_CP,nk_CP,l_quiet)
       case DEFAULT
          write(app_msg,*) 'invalid key '//trim(key)//' given to vgd_get (int 1D)'
-         call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+         if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
       
@@ -1522,7 +1521,7 @@ contains
       case ('VCRD')
          if (is_valid(self,"ip1_m_valid")) then
             write(app_msg,*) 'depricated key '//trim(key)//', use VCDM instead'
-            call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+            if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
             return
          else
             write(app_msg,*) 'Attempt to retrieve invalid key '//trim(key)//' returns VGD_MISSING'
@@ -1532,7 +1531,7 @@ contains
          endif
       case DEFAULT
          write(app_msg,*) 'invalid key '//trim(key)//' given to vgd_get (real 1D)'
-         call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+         if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
       
@@ -1659,7 +1658,7 @@ contains
          status = f_get_real8_1d(self%cptr,my_key//C_NULL_CHAR,value_CP,C_NULL_PTR,l_quiet)
       case DEFAULT
          write(app_msg,*) 'invalid key '//trim(key)//' given to vgd_get (real8 1D)'
-         call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+         if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
       return      
@@ -1710,7 +1709,7 @@ contains
          status = f_get_real8_3d(self%cptr,"VTBL"//C_NULL_CHAR,value_CP,C_NULL_PTR,C_NULL_PTR,C_NULL_PTR,l_quiet)         
       case DEFAULT
          write(app_msg,*) 'invalid key '//trim(key)//' given to vgd_get (real8 3D)'
-         call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
+         if (.not. quiet) call Lib_Log(APP_LIBVGRID,APP_ERROR,app_msg)       
          return
       end select
       
